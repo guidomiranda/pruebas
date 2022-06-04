c7d8e30dae243d8e560b7ee90b0d43f879ca3940
guido miranda <gmirce@gmail.com>
2022-06-03 23:29:20 -0400

    Build

---
+++ docs/precache-manifest.e22b18fb8234f6f68ac2ba34d840080b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa2e26a2d6297128eab535cb3603a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6885834d4224f1394d76",
+    "url": "/static/css/main.ec80207a.chunk.css"
+  },
+  {
+    "revision": "3ee90a7e13a60a5ce62d",
+    "url": "/static/js/2.6516e7c5.chunk.js"
+  },
+  {
+    "revision": "6885834d4224f1394d76",
+    "url": "/static/js/main.8c05c700.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c00ac42cf96f4fe10204a03d5220675c",
+    "url": "/static/media/bg.c00ac42c.png"
+  }
+]);
